15f605072bd18a23d2718f285c70ba81353c122c
Mustafa ILIKKAN <milikkan@yahoo.com>
2020-04-13 17:52:09 +0300

    complete course information exercise step4

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -29,27 +29,29 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ];
   
   return  (
     <div>
       <Header course={course} />
       <Content 
-        part1={part1.name} exercises1={part1.exercises} 
-        part2={part2.name} exercises2={part2.exercises} 
-        part3={part3.name} exercises3={part3.exercises} />
-      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+        part1={parts[0].name} exercises1={parts[0].exercises} 
+        part2={parts[1].name} exercises2={parts[1].exercises} 
+        part3={parts[2].name} exercises3={parts[2].exercises} />
+      <Total exercises1={parts[0].exercises} exercises2={parts[1].exercises} exercises3={parts[2].exercises} />
     </div>
   );
 }
