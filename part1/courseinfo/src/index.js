eea184aac07cae7a22fb0f6aaf86e43043407d72
Mustafa ILIKKAN <milikkan@yahoo.com>
2020-04-13 18:00:02 +0300

    modify course information exercise step4

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -7,23 +7,23 @@ const Header = (props) => {
 
 const Part = (props) => {
   return (
-    <p>{props.part} {props.exercises}</p>
+    <p>{props.name} {props.exercise}</p>
   );
 }
 
 const Content = (props) => {
   return (
     <>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+      <Part name={props.parts[0].name} exercise={props.parts[0].exercises} />
+      <Part name={props.parts[1].name} exercise={props.parts[1].exercises} />
+      <Part name={props.parts[2].name} exercise={props.parts[2].exercises} />
     </>
   );
 }
 
 const Total = (props) => {
   return (
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
   );
 }
 
@@ -47,11 +47,8 @@ const App = () => {
   return  (
     <div>
       <Header course={course} />
-      <Content 
-        part1={parts[0].name} exercises1={parts[0].exercises} 
-        part2={parts[1].name} exercises2={parts[1].exercises} 
-        part3={parts[2].name} exercises3={parts[2].exercises} />
-      <Total exercises1={parts[0].exercises} exercises2={parts[1].exercises} exercises3={parts[2].exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 }
