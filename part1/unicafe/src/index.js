f8fa47aaee90f37f492e2cf5d30b5b464d9979bd
Mustafa ILIKKAN <milikkan.dev@gmail.com>
2020-04-16 23:10:35 +0300

    complete unicafe exercise step3

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,15 +1,31 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const App = (props) => {
+const Statistics = (props) => {
+  const { good, neutral, bad } = props
+  const total = good + neutral + bad
+  const average = (good - bad) / total
+  const percentageOfPositives = (good / total) * 100
+  return (
+    <div>
+      <h2>statistics</h2>
+      good {good}<br />
+      neutral {neutral}<br />
+      bad {bad}<br />
+      all {total}<br />
+      average {average}<br />
+      positive {percentageOfPositives} %
+    </div>  
+  )
+}
+
+const App = () => {
   const [ good, setGood ] = useState(0);
   const [ neutral, setNeutral ] = useState(0);
   const [ bad, setBad ] = useState(0);
 
-  const total = good + neutral + bad
-  const average = (good - bad) / total
-  const percentageOfPositives = (good / total) * 100;
   
+
   return (
     <div>
       <h2>give feedback</h2>
@@ -22,13 +38,7 @@ const App = (props) => {
       <button onClick= { () => setBad(bad + 1) }>
         bad
       </button>
-      <h2>statistics</h2>
-      good {good}<br />
-      neutral {neutral}<br />
-      bad {bad}<br />
-      all {total}<br />
-      average {average}<br />
-      positive {percentageOfPositives} %
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>  
   )
 }
